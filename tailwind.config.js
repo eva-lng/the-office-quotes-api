4f57da87e7c9fa65954af9e44419ed89aa9d2b65
Eva Langerova <langerova_eva@yahoo.com>
2023-07-19 14:05:57 +0200

    changed bg image css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'office': "linear-gradient(to right bottom, rgba(4, 9, 30 ,0.7), rgba(4, 9, 30 ,0.9)), url('../assets/officebg02.jpg')"
+        'office': "linear-gradient(to right bottom, rgba(4, 9, 30 ,0.7), rgba(4, 9, 30 ,0.9)), url('../assets/officebg.jpg')"
       }
     }
   },
